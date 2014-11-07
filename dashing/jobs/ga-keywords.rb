2904abf34b5c60b53e62200d1751bb2537057448
Henry Rose <henryr@porch.com>
2014-11-07 09:20:13 -0800

    Hardcodes GA values

--- dashing/jobs/ga-keywords.rb
+++ dashing/jobs/ga-keywords.rb
@@ -5,24 +5,66 @@ require "json"
 
 SCHEDULER.every '2s' do
 
-  uri = URI.parse("http://porch-ga-proxy.appspot.com/query?id=ahBzfnBvcmNoLWdhLXByb3h5chULEghBcGlRdWVyeRiAgICA3pCBCgw")
+  #uri = URI.parse("http://porch-ga-proxy.appspot.com/query?id=ahBzfnBvcmNoLWdhLXByb3h5chULEghBcGlRdWVyeRiAgICA3pCBCgw")
 
-  response = Net::HTTP.get_response(uri)
+  #response = Net::HTTP.get_response(uri)
 	
-  json = JSON.parse(response.body)
+  #json = JSON.parse(response.body)
   
-  puts response.body
+  #puts response.body
   
-  data = json["rows"]
+  #data = json["rows"]
   
   keywords = Array.new
   
-  data.each do |keyword|
+  #data.each do |keyword|
+  #  keywords.push({
+  #    label: keyword[0],
+  #    value: keyword[1]
+  #  })
+  #end
+  
+  keywords.push({
+	label: "(not provided)", 
+	value: "1265289"
+  })
+  keywords.push({
+	label: "(not provided", 
+	value: "170209"
+  })
     keywords.push({
-      label: keyword[0],
-      value: keyword[1]
-    })
-  end
+	label: "porch.com", 
+	value: "16327"
+  })
+    keywords.push({
+	label: "porch", 
+	value: "4465"
+  })
+    keywords.push({
+	label: "porch .com lowes", 
+	value: "1502"
+  })
+    keywords.push({
+	label: "www.porch.com", 
+	value: "1237"
+  })
+    keywords.push({
+	label: "plomari general contractors", 
+	value: "491"
+  })
+    keywords.push({
+	label: "pro.porch.com", 
+	value: "447"
+  })
+    keywords.push({
+	label: "porch.com/lowes", 
+	value: "372"
+  })
+    keywords.push({
+	label: "porch .com contractors", 
+	value: "312"
+  })
+
      
   send_event('ga_top_keywords', { items: keywords })
   
--- dashing/jobs/ga.rb
+++ dashing/jobs/ga.rb
@@ -2,19 +2,21 @@ require "net/http"
 require "uri"
 require "json"
 
-current_organic_visitors = 0; 
+current_organic_visitors = 90726; 
 
 SCHEDULER.every '2s' do
 
-  uri = URI.parse("http://porch-ga-proxy.appspot.com/query?id=ahBzfnBvcmNoLWdhLXByb3h5chULEghBcGlRdWVyeRiAgICAvKGCCgw")
+  # uri = URI.parse("http://porch-ga-proxy.appspot.com/query?id=ahBzfnBvcmNoLWdhLXByb3h5chULEghBcGlRdWVyeRiAgICAvKGCCgw")
 
-  response = Net::HTTP.get_response(uri)
+  # response = Net::HTTP.get_response(uri)
 	
-  json = JSON.parse(response.body)
+  # json = JSON.parse(response.body)
   
-  puts response.body
+  # puts response.body
   
-  new_organic_visitors = json["rows"][0][0]
+  # new_organic_visitors = json["rows"][0][0]
+
+  new_organic_visitors = current_organic_visitors + 100
 
   last_organic_visitors = current_organic_visitors
 
