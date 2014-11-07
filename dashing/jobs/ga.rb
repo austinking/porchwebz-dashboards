4af1ac992fb957c74eb590b6b14673bdcb0e24c6
Henry Rose <henryr@porch.com>
2014-11-06 16:53:49 -0800

    Adds GA Organic Visitors widget

---
+++ dashing/jobs/ga.rb
@@ -0,0 +1,24 @@
+require "net/http"
+require "uri"
+require "json"
+
+current_organic_visitors = 0; 
+
+SCHEDULER.every '2s' do
+
+  uri = URI.parse("http://porch-ga-proxy.appspot.com/query?id=ahBzfnBvcmNoLWdhLXByb3h5chULEghBcGlRdWVyeRiAgICAvKGCCgw")
+
+  response = Net::HTTP.get_response(uri)
+	
+  json = JSON.parse(response.body)
+  
+  puts response.body
+  
+  new_organic_visitors = json["rows"][0][0]
+
+  last_organic_visitors = current_organic_visitors
+
+  current_organic_visitors = new_organic_visitors
+  
+  send_event('organic_visitors', { current: current_organic_visitors, last: last_organic_visitors })
+end
