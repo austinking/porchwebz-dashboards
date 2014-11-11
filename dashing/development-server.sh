bb31f0838b526d2087098efc9af5ea071eca0c3a
Henry Rose <henryr@porch.com>
2014-11-11 15:29:58 -0800

    Updates development-server start script for easier dev cycles

--- dashing/development-server.sh
+++ dashing/development-server.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 DASHING_HOME=`pwd`
-echo $DASHING_HOME
 docker run -d \
+    --name=porch-dashboard \
     -v=$DASHING_HOME/assets:/dashing/assets \
     -v=$DASHING_HOME/config:/dashing/config \
     -v=$DASHING_HOME/dashboards:/dashing/dashboards \
     -v=$DASHING_HOME/jobs:/dashing/jobs \
     -v=$DASHING_HOME/widgets:/dashing/widgets \
-    -p=3030:3030 $DASHING_DEV
+    -p=3030:3030 porch-dashing:dev
